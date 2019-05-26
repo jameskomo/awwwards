347618c433f98699726c5ac5a6cd516f7d0054ab
jameskomo <james.komoh@gmail.com>
2019-05-26 21:16:03 +0300

    Profile API model created and model properties defined

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class Profile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image=models.ImageField(default="profile_pics/default.jpeg", upload_to='profile_pics')
@@ -21,29 +22,11 @@ class Profile(models.Model):
 
 
 class ProfileApi(models.Model):
-    project_title=models.CharField(max_length=60)
-    project_description = models.TextField()
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image=models.ImageField(default="profile_pics/default.jpeg", upload_to='profile_pics')
+    bio=models.CharField(max_length=200, blank=False)
+    projects_posted=models.ForeignKey(Profile, on_delete=models.CASCADE)
+    contact_information = models.TextField()
 
     
-
-    def __str__(self):
-        return f'{self.user.username} Profile'
-
-class Follow(models.Model):
-    username = models.ForeignKey(User, related_name='follower', on_delete=models.CASCADE)
-    followed = models.ForeignKey(User, related_name='followed', on_delete=models.CASCADE)
-    follow_id = models.CharField(max_length=50,unique=True, null=True)
-
-    def __str__(self):
-        return self.follow_id
-
-    def save_like(self):
-        self.save()
-
-
-
-
-
-
 
