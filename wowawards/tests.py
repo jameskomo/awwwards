36c8ee3ca49c239a51c7cc1fb70a22689f1f4ddf
jameskomo <james.komoh@gmail.com>
2019-05-27 10:51:06 +0300

    Tests redone for both Profile and Projects models

--- wowawards/tests.py
+++ wowawards/tests.py
@@ -1,14 +1,17 @@
 from django.test import TestCase
 import datetime as dt
-from .models import Image, Profile
+from .models import Image, Profile, Ratings
 
 class ImageTestClass(TestCase):
     # Set up method
     def setUp(self):
-        self.image1= Image(image_name = 'Nduma', image_caption ='African Tastes', image_comments ='Food', pub_date ='May 10, 2019, 5:28 p.m.', photo_image='media/images/food.jpg')
+        self.project1= Image(project_title='Test', project_description='Test Project Instances', pub_date='May 27, 2019', project_image='media/websites/website1.jpg', project_link='www.google.com')
     # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.image1,Image))
+        self.assertTrue(isinstance(self.project1,Image))
+
+
+    
 
     # def test_save_method(self):
     #     self.image1.save_image()
@@ -19,12 +22,12 @@ class ImageTestClass(TestCase):
     def tearDown(self):
         Image.objects.all().delete()
         Profile.objects.all().delete()
+        Ratings.objects.all().delete()
     
 class ProfileTestClass(TestCase):
     # Set up method
     def setUp(self):
-        self.profile1= Profile(profile_image='media/images/food.jpg', bio='here to create')
-    # Testing  instance
+        self.profile1= Profile(profile_image='media/profile_pic/komo2.jpg', bio='here to create')
     def test_instance(self):
         self.assertTrue(isinstance(self.profile1,Profile))
 
